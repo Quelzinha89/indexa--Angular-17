677b101305aad5516df9fa582df6b8b1f1a3d303
Raquel Correa <raquelfilosofiamackenzie@gmail.com>
2024-06-14 10:28:33 -0300

    Projeto Angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,55 @@
+import { ContainerComponent } from './componentes/container/container.component';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+
+interface Contato {
+  id: number
+  nome: string
+  telefone: string
+}
+
+import agenda from './agenda.json'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos : Contato[] = agenda
+
+  filtroPoTexto: string = ''
+
+  filtrarContatosPorTexto(): Contato [] {
+    if(!this.filtroPoTexto){
+      return this.contatos
+    }
+
+    return this.contatos.filter(contato => {
+      return contato.nome.toLocaleUpperCase().includes (this.filtroPoTexto.toLocaleLowerCase())
+    })
+  }
+
+  filtrarContatosPorLetraInicial (letras:string) :  Contato [] {
+      return this.contatos.filter ( contato => {
+        return contato.nome.toLocaleLowerCase().startsWith(letras)
+      } )
+  }
 }
+
